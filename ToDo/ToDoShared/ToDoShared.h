53b3793aff4aee743fbd0d23d819afde0ce37e3d
Pankaj Sachdeva <dearestpankaj@gmail.com>
2024-03-05 14:05:48 +0100

    Add initial xcode project

---
+++ ToDo/ToDoShared/ToDoShared.h
@@ -0,0 +1,18 @@
+//
+//  ToDoShared.h
+//  ToDoShared
+//
+//  Created by Pankaj Sachdeva on 05.03.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ToDoShared.
+FOUNDATION_EXPORT double ToDoSharedVersionNumber;
+
+//! Project version string for ToDoShared.
+FOUNDATION_EXPORT const unsigned char ToDoSharedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToDoShared/PublicHeader.h>
+
+
